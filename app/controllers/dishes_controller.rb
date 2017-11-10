aa009068446fa6391d69cafadfc68451ea6729b4
josephpung <goldenpummel@live.com>
2017-11-11 01:13:40 +0800

    added rating to individual dish page

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -6,5 +6,19 @@ class DishesController < ApplicationController
   def show
     @dish = Dish.find(params[:id])
     @resto = Restaurant.find(@dish.restaurant_id)
+    @all_ratings = Rating.all
+
+    @ratingHash = {}
+
+    @all_ratings.each do |rating|
+    if @ratingHash[rating.dish_id]
+          @ratingHash[rating.dish_id] +=1
+
+    else
+      @ratingHash[rating.dish_id] = 1
+
+    end
+  end
+  @dishRating = @ratingHash[@dish.id]
   end
 end

f3238c76fd58c41b2e964dd49efed4050c9a8e7d
josephpung <goldenpummel@live.com>
2017-11-13 16:57:25 +0800

    changed css and moved searchbar to partials

--- app/assets/javascripts/frontend.js
+++ app/assets/javascripts/frontend.js
@@ -34,4 +34,5 @@ $(document).on('turbolinks:load', function () {
   $('#textarea1').val('New Text');
   $('#textarea1').trigger('autoresize');
    $('select').material_select();
+   $('.parallax').parallax();
 })
